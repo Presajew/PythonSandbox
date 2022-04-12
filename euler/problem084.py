5fa4b35d6fce58f678cbe7245135c7ff8404f11c
Presajew <shannonmladd@gmail.com>
2022-04-12 11:49:33 -0600

    formatting

--- euler/problem084.py
+++ euler/problem084.py
@@ -58,215 +58,221 @@ import random
 
 
 def calculate() -> int:
-	
-	# current location
-	location = 0
 
-	# double counter
-	double_count = 0
+    # current location
+    location = 0
 
-	# init game
-	board = init_board()
-	chance = init_chance()
-	community_chest = init_community_chest()
+    # double counter
+    double_count = 0
 
-	# test dice
-	for y in range(1000000):
+    # init game
+    board = init_board()
+    chance = init_chance()
+    community_chest = init_community_chest()
 
-		# roll check
-		dice = roll()
+    # test dice
+    for y in range(1000000):
 
-		# double check
-		if is_double(dice):
-			double_count += 1
-		else:
-			double_count = 0
+        # roll check
+        dice = roll()
 
-		# if 3 doubles, go to jail
-		if double_count == 3:		
-			location = 10
-			board[location][1] += 1
-			double_count = 0
-			continue
+        # double check
+        if is_double(dice):
+            double_count += 1
+        else:
+            double_count = 0
 
-		location = move(location, dice)
+        # if 3 doubles, go to jail
+        if double_count == 3:
+            location = 10
+            board[location][1] += 1
+            double_count = 0
+            continue
 
-		# lands on go to jail
-		if location == 30:
-			location = 10
-			board[location][1] += 1
-			continue
-		
-		# lands on chance
-		if is_chance(location):
-			#print(f"{dice} !!! You landed on chance")
-			if len(chance) == 0:
-				chance = init_chance()
-			card = chance.pop()
-			move_modifier = draw_chance(card, location)
-			if move_modifier != -1:
-				location = move_modifier
-			board[location][1] += 1
-			continue
-		
-		# lands on community chest
-		if is_community_chest(location):
-			#print(f"{dice} !!! You landed on community chest")
-			if len(community_chest) == 0:
-				community_chest = init_community_chest()
-			card = community_chest.pop()
-			move_modifier = draw_community_chest(card)
-			if move_modifier != -1:
-				location = move_modifier
-			board[location][1] += 1
-			continue
+        location = move(location, dice)
 
-		# all other scenarios
-		board[location][1] += 1
-			
+        # lands on go to jail
+        if location == 30:
+            location = 10
+            board[location][1] += 1
+            continue
 
-		
+        # lands on chance
+        if is_chance(location):
+            # print(f"{dice} !!! You landed on chance")
+            if len(chance) == 0:
+                chance = init_chance()
+            card = chance.pop()
+            move_modifier = draw_chance(card, location)
+            if move_modifier != -1:
+                location = move_modifier
+            board[location][1] += 1
+            continue
 
-	print(board)
-	print(f"\n{get_top_3(board)}")
-	return -1
+        # lands on community chest
+        if is_community_chest(location):
+            # print(f"{dice} !!! You landed on community chest")
+            if len(community_chest) == 0:
+                community_chest = init_community_chest()
+            card = community_chest.pop()
+            move_modifier = draw_community_chest(card)
+            if move_modifier != -1:
+                location = move_modifier
+            board[location][1] += 1
+            continue
+
+        # all other scenarios
+        board[location][1] += 1
+
+    print(board)
+    print(f"\n{get_top_3(board)}")
+    return -1
 
 
 def roll() -> list:
-	dice = []
-	#print("\nRolling...")
-	dice.append(random.randint(1,4))
-	dice.append(random.randint(1,4))
-	#print(f"--> {dice}")
-	return dice
+    dice = []
+    # print("\nRolling...")
+    dice.append(random.randint(1, 4))
+    dice.append(random.randint(1, 4))
+    # print(f"--> {dice}")
+    return dice
+
 
 def is_double(dice) -> bool:
-	output = dice[0] == dice[1]
-	#if output:
-	#	print(f"{dice} is a double -> player would roll again")
-	#else:
-	#	print(f"{dice} is not double -> end of turn")
-	return output
+    output = dice[0] == dice[1]
+    # if output:
+    # 	print(f"{dice} is a double -> player would roll again")
+    # else:
+    # 	print(f"{dice} is not double -> end of turn")
+    return output
+
 
 def roll_total(dice) -> int:
-	output = sum(dice)
-	#print(f"{dice} results in a movement of {output}")
-	return output
+    output = sum(dice)
+    # print(f"{dice} results in a movement of {output}")
+    return output
+
 
 def init_board() -> list:
-	output = []
-	for i in range(40):
-		tile = [i, 0]
-		output.append(tile)
-	return output
+    output = []
+    for i in range(40):
+        tile = [i, 0]
+        output.append(tile)
+    return output
+
 
 def move(current_location, dice) -> int:
-	output = current_location + roll_total(dice)
-	if (output > 39):
-		output -= 40
-	return output
-	
+    output = current_location + roll_total(dice)
+    if output > 39:
+        output -= 40
+    return output
+
+
 def is_chance(location) -> bool:
-	chance_locations = [7, 22, 36]
-	return location in chance_locations
+    chance_locations = [7, 22, 36]
+    return location in chance_locations
+
 
 def is_community_chest(location) -> bool:
-	chance_locations = [2, 17, 33]
-	return location in chance_locations
+    chance_locations = [2, 17, 33]
+    return location in chance_locations
+
 
 def init_chance() -> list:
-	output = [
-		"Advance to GO",
-		"Go to JAIL",
-		"Go to C1",
-		"Go to E3",
-		"Go to H2",
-		"Go to R1",
-		"Go to next Railroad",
-		"Go to next Railroad",
-		"Go to next Utility",
-		"Go back 3 squares."
-	]
-	for i in range(6):
-		output.append("Nothing")
-	random.shuffle(output)
-	return output
+    output = [
+        "Advance to GO",
+        "Go to JAIL",
+        "Go to C1",
+        "Go to E3",
+        "Go to H2",
+        "Go to R1",
+        "Go to next Railroad",
+        "Go to next Railroad",
+        "Go to next Utility",
+        "Go back 3 squares.",
+    ]
+    for i in range(6):
+        output.append("Nothing")
+    random.shuffle(output)
+    return output
+
 
 def init_community_chest() -> list:
-	output = [
-		"Advance to GO",
-		"Go to JAIL"
-	]
-	for i in range(14):
-		output.append("Nothing")
-	random.shuffle(output)
-	return output
+    output = ["Advance to GO", "Go to JAIL"]
+    for i in range(14):
+        output.append("Nothing")
+    random.shuffle(output)
+    return output
+
 
 def draw_chance(card, current_location) -> int:
-	#print(f"Chance drawn was {card}")
-	if card == "Advance to GO":
-		return 0
-	elif card == "Go to JAIL":
-		return 10
-	elif card == "Go to C1":
-		return 11
-	elif card == "Go to E3":
-		return 24
-	elif card == "Go to H2":
-		return 39
-	elif card == "Go to R1":
-		return 5
-	elif card == "Go to next Railroad":
-		output = current_location
-		while True:
-			if output % 5 == 0 and output % 2 == 1:
-				return output
-			output += 1
-			if output == 40:
-				output = 0
-	elif card == "Go to next Utility":
-		if current_location > 12 and current_location <= 28:
-			return 28
-		else:
-			return 12
-	elif card == "Go back 3 squares":
-		output = current_location - 3
-		if output < 0:
-			output += 40
-		return output
-	else:
-		return -1
+    # print(f"Chance drawn was {card}")
+    if card == "Advance to GO":
+        return 0
+    elif card == "Go to JAIL":
+        return 10
+    elif card == "Go to C1":
+        return 11
+    elif card == "Go to E3":
+        return 24
+    elif card == "Go to H2":
+        return 39
+    elif card == "Go to R1":
+        return 5
+    elif card == "Go to next Railroad":
+        output = current_location
+        while True:
+            if output % 5 == 0 and output % 2 == 1:
+                return output
+            output += 1
+            if output == 40:
+                output = 0
+    elif card == "Go to next Utility":
+        if current_location > 12 and current_location <= 28:
+            return 28
+        else:
+            return 12
+    elif card == "Go back 3 squares":
+        output = current_location - 3
+        if output < 0:
+            output += 40
+        return output
+    else:
+        return -1
+
 
 def draw_community_chest(card) -> int:
-	#print(f"Community Chest drawn was {card}")
-	if card == "Advance to GO":
-		return 0
-	elif card == "Go to JAIL":
-		return 10
-	else:
-		return -1
+    # print(f"Community Chest drawn was {card}")
+    if card == "Advance to GO":
+        return 0
+    elif card == "Go to JAIL":
+        return 10
+    else:
+        return -1
+
 
 def get_top_3(board) -> list:
-	top_3 = [
-		[0,0],
-		[0,0],
-		[0,0]
-	]
-	for square in board:
-		if square[1] > top_3[2][1]:
-			top_3[2] = square
-		if square[1] > top_3[1][1]:
-			top_3[2] = top_3[1]
-			top_3[1] = square
-		if square[1] > top_3[0][1]:
-			top_3[1] = top_3[0]
-			top_3[0] = square
+    top_3 = [[0, 0], [0, 0], [0, 0]]
+    for square in board:
+        if square[1] > top_3[2][1]:
+            top_3[2] = square
+        if square[1] > top_3[1][1]:
+            top_3[2] = top_3[1]
+            top_3[1] = square
+        if square[1] > top_3[0][1]:
+            top_3[1] = top_3[0]
+            top_3[0] = square
 
-	print(str(top_3[0][0]).zfill(2) + str(top_3[1][0]).zfill(2) + str(top_3[2][0]).zfill(2))
-	return top_3
+    print(
+        str(top_3[0][0]).zfill(2)
+        + str(top_3[1][0]).zfill(2)
+        + str(top_3[2][0]).zfill(2)
+    )
+    return top_3
+
 
 if __name__ == "__main__":
-	start_time = time.time()
-	print(f"The answer to Problem 84 is {calculate()}")
-	end_time = time.time()
-	print(f"This solution took {end_time - start_time} seconds to run")
+    start_time = time.time()
+    print(f"The answer to Problem 84 is {calculate()}")
+    end_time = time.time()
+    print(f"This solution took {end_time - start_time} seconds to run")
